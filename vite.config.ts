ec5843c96b5458f772f41652370a0b10d2d13bc1
xampe11 <juampi.farinia@gmail.com>
2025-05-22 15:22:15 -0300

    Finalizing updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [react(), runtimeErrorOverlay()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve("front-end", "src"),
